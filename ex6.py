ba2bd277654f5ed1fa1f82140af5338f6a26c4eb
Brettyoke49 <Bmprobe@g.clemson.edu>
2019-01-31 19:20:27 -0500

    Examples

---
+++ ex6.py
@@ -0,0 +1,13 @@
+y = 10
+
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_eval = "Isn't that joke so funny?! {}"
+
+print(joke_eval.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
